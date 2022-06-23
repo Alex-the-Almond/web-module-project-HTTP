a15eaa3809531ed11ffad2c8422f1fe116c264c4
Alexander Aymond <lynxkhaos@gmail.com>
2022-06-23 13:00:02 -0500

    complete

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import MovieList from './components/MovieList';
 import Movie from './components/Movie';
 
 import MovieHeader from './components/MovieHeader';
-
+import EditMovieForm from './components/EditMovieForm'
 import FavoriteMovieList from './components/FavoriteMovieList';
-
+import AddMovieForm from './components/AddMovieForm';
 import axios from 'axios';
 
 const App = (props) => {
@@ -25,6 +25,7 @@ const App = (props) => {
   }, []);
 
   const deleteMovie = (id)=> {
+    setMovies(movies.filter(item =>(item.id !== Number(id))));
   }
 
   const addToFavorites = (movie) => {
@@ -44,10 +45,15 @@ const App = (props) => {
         
           <Switch>
             <Route path="/movies/edit/:id">
+              <EditMovieForm setMovies={setMovies}/>
+            </Route>
+
+            <Route path='/movies/add'>
+              <AddMovieForm setMovies={setMovies} />
             </Route>
 
             <Route path="/movies/:id">
-              <Movie/>
+              <Movie deleteMovie={deleteMovie}/>
             </Route>
 
             <Route path="/movies">
